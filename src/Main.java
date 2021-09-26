c8229a3c7a1372d0449d1a0581f81877385a0e1e
Borchardt62640 <piotr3borchardt@gmail.com>
2021-09-26 13:46:13 +0200

    cw 6

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,15 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
+        Scanner scan = new Scanner(System.in);
+        int[] tab = new int[5];
 
+        for(int i = 0; i < tab.length; i++){
+            tab[i] = scan.nextInt();
+        }
+
+        for(int i = 0; i < tab.length; i++){
+            System.out.println("tab[" + i + "] = " + tab[i]);
+        }
     }
 }
