ab4c9ecde8a7fb6cceb393383f8f6d3ffbc56f22
Borchardt62640 <piotr3borchardt@gmail.com>
2021-09-26 12:19:35 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("cw1 ");
+        System.out.println("ale "+"super");
+
+        System.out.printf("ale %s dalej %d super.\n", "cos", 1);
+        System.out.print("znowu");
+        System.out.print(" cos");
+        System.out.print(" napisalem.");
         //test
     }
 
