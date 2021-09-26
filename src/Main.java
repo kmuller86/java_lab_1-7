8d9bce27972729bbf0e1b7eb625b47763427c7f8
Borchardt62640 <piotr3borchardt@gmail.com>
2021-09-26 13:10:16 +0200

    cw 4

--- src/Main.java
+++ src/Main.java
@@ -4,43 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* ćw.3 */
+        /* ćw.4 */
 
-        /*  zad.1 napisać interfejs który bedzie słuzył do wpisania danych osobowych:
-         *   a) Imię, nazwisko, wiek, nr indeksu,
-         *   dane mają być wprowadzane z klawiatury w konsoli
-         *   b) wyświetlić dane za pomocą println i printf
-         */
-            String imie;
-            String nazwisko;
-            int wiek;
-            int index;
+        /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+        boolean a = false;
+        boolean b = false;
+        boolean c = true;
 
-            Scanner scan = new Scanner(System.in);
-            System.out.println("podaj imie");
-            imie = scan.next();
-            System.out.println("podaj nazwisko");
-            nazwisko = scan.next();
-            System.out.println("podaj wiek");
-            wiek = scan.nextInt();
-            System.out.println("podaj indeks");
-            index = scan.nextInt();
+        int x = 45;
+        int y = 223;
+        int z = 7;
 
-            System.out.println("imie = " + imie);
-            System.out.println("nazwisko = " + nazwisko);
-            System.out.printf("wiek = " + wiek + "\n");
-            System.out.printf("indeks = " + index + "\n");
 
-        /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
-         *   wynik wyświetlić w konsoli programu
-         */
-        System.out.println("podaj 2 liczby");
-        int x = scan.nextInt();
-        int y = scan.nextInt();
-        System.out.println("+ " + (x + y));
-        System.out.println("- " + (x - y));
-        System.out.println("* " + (x * y));
-        System.out.println("/ " + (x / y));
-        System.out.println("% " + (x % y));
+
+        System.out.println("!(((a && c) || (!a || b) && (false || c )) || (!(x < z) && (y == x) && !(z <= y)))) : " + !(
+                ((a && c) || (!a || b) && (false || c )) || (!(x < z) && (y == x) && !(z <= y))
+        )); // false
+
     }
 }
